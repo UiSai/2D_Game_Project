f2535eacef0ee174e3f64f1f34cc4b41be3438f5
UiSai <ghju6780@outlook.kr>
2018-11-22 14:34:38 +0900

    2번째 지형 추가
    프로그램 내에서는 아직 접근할 수 없습니다.

--- stage1_BG.py
+++ stage1_BG.py
@@ -3,6 +3,41 @@ from pico2d import *
 import game_world
 import game_framework
 
+
+class Block:
+    @staticmethod
+    def enter(background):
+        pass
+
+    @staticmethod
+    def exit(background):
+        pass
+
+    @staticmethod
+    def do(background):
+        pass
+
+    @staticmethod
+    def draw(background):
+        if background.block == 1:
+            background.tree_1.draw(250, background.first_floor_of_tree)
+            background.tree_2.draw(1000, background.first_floor_of_tree)
+
+            background.grass.draw(150, background.first_floor_of_grass)
+            background.grass.draw(450, background.first_floor_of_grass)
+            background.grass.draw(700, background.first_floor_of_grass)
+            background.grass.draw(950, background.first_floor_of_grass)
+            background.grass.draw(1200, background.first_floor_of_grass)
+        elif background.block == 2:
+            background.tree_2.draw(1000, background.first_floor_of_tree)
+
+            background.grass.draw(150, background.first_floor_of_grass)
+            background.grass.draw(450, background.first_floor_of_grass)
+            background.grass.draw(700, background.first_floor_of_grass)
+            background.grass.draw(950, background.first_floor_of_grass)
+            background.grass.draw(1200, background.first_floor_of_grass)
+
+
 class Background:
     def __init__(self):
         self.grass = load_image('resource\\Background\\d_grass.png')
@@ -10,16 +45,10 @@ class Background:
         self.tree_2 = load_image('resource\\Background\\tree2.png')
         self.first_floor_of_grass = 40
         self.first_floor_of_tree = 460
+        self.block = 1
 
     def draw(self):
-        self.tree_1.draw(250, self.first_floor_of_tree)
-        self.tree_2.draw(1000, self.first_floor_of_tree)
-
-        self.grass.draw(150, self.first_floor_of_grass)
-        self.grass.draw(450, self.first_floor_of_grass)
-        self.grass.draw(700, self.first_floor_of_grass)
-        self.grass.draw(950, self.first_floor_of_grass)
-        self.grass.draw(1200, self.first_floor_of_grass)
+        Block.draw(self)
 
     def update(self):
         pass
