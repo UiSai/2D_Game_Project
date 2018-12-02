f3ce14646af2244b975b565ca5de298e2872fa0b
UiSai <ghju6780@outlook.kr>
2018-12-02 21:19:12 +0900

    배경 변경 완료
    근데 비율이 생각보다 너무 크다....
    
    알려진 문제
    블록 4에서 착지할 시 자동으로 땅끝으로 고정됩니다.

--- stage1_BG.py
+++ stage1_BG.py
@@ -18,51 +18,17 @@ class Block:
     def draw(background):
         if background.block == 1:
             background.BG1.draw(640, 480)
-            background.tree_2.draw(1000, background.first_floor_of_tree)
-
-            background.grass.draw(150, background.first_floor_of_grass)
-            background.grass.draw(450, background.first_floor_of_grass)
-            background.grass.draw(700, background.first_floor_of_grass)
-            background.grass.draw(950, background.first_floor_of_grass)
-            background.grass.draw(1200, background.first_floor_of_grass)
         elif background.block == 2:
             background.BG2.draw(640, 480)
-            background.tree_2.draw(1000, background.first_floor_of_tree)
-
-            background.grass.draw(150, background.first_floor_of_grass)
-            background.grass.draw(450, background.first_floor_of_grass)
-            background.grass.draw(700, background.first_floor_of_grass)
-            background.grass.draw(950, background.first_floor_of_grass)
-            background.grass.draw(1200, background.first_floor_of_grass)
         elif background.block == 3:  # 올라가는 길
             background.BG3.draw(640, 480)
-            background.grass.draw(150, background.first_floor_of_grass)
-            background.grass.draw(450, background.first_floor_of_grass)
-            background.grass.draw(700, background.first_floor_of_grass)
-            background.grass.draw(950, background.first_floor_of_grass)
-            background.grass.draw(1200, background.first_floor_of_grass)
-
-            background.wall.draw(1100, 200)
         elif background.block == 4:  # 올라온 길
             background.BG4.draw(640, 480)
-            background.grass.draw(700, background.first_floor_of_grass)
-            background.grass.draw(950, background.first_floor_of_grass)
-            background.grass.draw(1200, background.first_floor_of_grass)
-
         elif background.block == 5:  # 우두머리 직전
             background.BG5.draw(640, 480)
-            background.grass.draw(150, background.first_floor_of_grass)
-            background.grass.draw(450, background.first_floor_of_grass)
-            background.grass.draw(700, background.first_floor_of_grass)
-            background.grass.draw(950, background.first_floor_of_grass)
-            background.grass.draw(1200, background.first_floor_of_grass)
         elif background.block == 6:  # 우두머리
             background.BG6.draw(640, 480)
-            background.grass.draw(150, background.first_floor_of_grass)
-            background.grass.draw(450, background.first_floor_of_grass)
-            background.grass.draw(700, background.first_floor_of_grass)
-            background.grass.draw(950, background.first_floor_of_grass)
-            background.grass.draw(1200, background.first_floor_of_grass)
+
 
 class Background:
     def __init__(self):
@@ -72,11 +38,9 @@ class Background:
         self.BG4 = load_image('resource\\Background\\BG4.png')
         self.BG5 = load_image('resource\\Background\\BG5.png')
         self.BG6 = load_image('resource\\Background\\BG6.png')
-        self.BG6c = load_image('resource\\Background\\BG6c.png')
         self.first_floor_of_grass = 40
         self.first_floor_of_tree = 460
-        self.block = 6
-        self.prev_block = 1
+        self.block = 1
 
         if not title_state.easter:
             self.bgm = load_music('resource\\Sound\\bgm.mp3')
