95dae5faba580b0f5d22eedbede08184ba6c3139
UiSai <ghju6780@outlook.kr>
2018-12-02 20:01:57 +0900

    배경 변경
    BG1, BG2 파일 업로드.
    변경된 배경을 표시하기 위해 코드를 수정했습니다.

--- stage1_BG.py
+++ stage1_BG.py
@@ -17,7 +17,7 @@ class Block:
     @staticmethod
     def draw(background):
         if background.block == 1:
-            background.tree_1.draw(250, background.first_floor_of_tree)
+            background.BG1.draw(640, 480)
             background.tree_2.draw(1000, background.first_floor_of_tree)
 
             background.grass.draw(150, background.first_floor_of_grass)
@@ -26,6 +26,7 @@ class Block:
             background.grass.draw(950, background.first_floor_of_grass)
             background.grass.draw(1200, background.first_floor_of_grass)
         elif background.block == 2:
+            background.BG2.draw(640, 480)
             background.tree_2.draw(1000, background.first_floor_of_tree)
 
             background.grass.draw(150, background.first_floor_of_grass)
@@ -34,6 +35,7 @@ class Block:
             background.grass.draw(950, background.first_floor_of_grass)
             background.grass.draw(1200, background.first_floor_of_grass)
         elif background.block == 3:  # 올라가는 길
+            background.BG3.draw(640, 480)
             background.grass.draw(150, background.first_floor_of_grass)
             background.grass.draw(450, background.first_floor_of_grass)
             background.grass.draw(700, background.first_floor_of_grass)
@@ -42,17 +44,20 @@ class Block:
 
             background.wall.draw(1100, 200)
         elif background.block == 4:  # 올라온 길
+            background.BG4.draw(640, 480)
             background.grass.draw(700, background.first_floor_of_grass)
             background.grass.draw(950, background.first_floor_of_grass)
             background.grass.draw(1200, background.first_floor_of_grass)
 
         elif background.block == 5:  # 우두머리 직전
+            background.BG5.draw(640, 480)
             background.grass.draw(150, background.first_floor_of_grass)
             background.grass.draw(450, background.first_floor_of_grass)
             background.grass.draw(700, background.first_floor_of_grass)
             background.grass.draw(950, background.first_floor_of_grass)
             background.grass.draw(1200, background.first_floor_of_grass)
         elif background.block == 6:  # 우두머리
+            background.BG6.draw(640, 480)
             background.grass.draw(150, background.first_floor_of_grass)
             background.grass.draw(450, background.first_floor_of_grass)
             background.grass.draw(700, background.first_floor_of_grass)
@@ -61,10 +66,13 @@ class Block:
 
 class Background:
     def __init__(self):
-        self.grass = load_image('resource\\Background\\grass_modify.png')
-        self.tree_1 = load_image('resource\\Background\\tree.png')
-        self.tree_2 = load_image('resource\\Background\\tree2.png')
-        self.wall = load_image('resource\\Background\\wall.png')
+        self.BG1 = load_image('resource\\Background\\BG1.png')
+        self.BG2 = load_image('resource\\Background\\BG2.png')
+        self.BG3 = load_image('resource\\Background\\BG3.png')
+        self.BG4 = load_image('resource\\Background\\BG4.png')
+        self.BG5 = load_image('resource\\Background\\BG5.png')
+        self.BG6 = load_image('resource\\Background\\BG6.png')
+        self.BG6c = load_image('resource\\Background\\BG6c.png')
         self.first_floor_of_grass = 40
         self.first_floor_of_tree = 460
         self.block = 6
